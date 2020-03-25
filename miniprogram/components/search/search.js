bf5e56813cead5d3c18c4e21fd2c4dff19b0708a
iKEMS <xingjie0014@163.com>
2020-03-26 00:14:47 +0800

    blog header

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -4,9 +4,13 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    placeHolder: {
+      type: String,
+      value: '请输入关键字'
+    }
   },
 
+  externalClasses: ['iconfont', 'icon-sousuo'],
   /**
    * 组件的初始数据
    */
